b537feecba5a9acffaa85b2198243cb18ab416db
shuaixinjie <shuaixinjie@foxmail.com>
2021-07-12 15:32:15 +0800

    commit

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"grade_system/csv"
 	"grade_system/memory"
@@ -18,6 +19,12 @@ func main() {
 	http.HandleFunc("/uploadMore", uploadMore)
 	http.HandleFunc("/query/one", queryOne)
 	http.HandleFunc("/query/condition", queryCondition)
+	// 录入单个学生成绩
+	http.HandleFunc("/student", addScore)
+	http.HandleFunc("/query/average", queryAvg)
+	http.HandleFunc("/query/maxmin", queryMaxOrMin)
+	http.HandleFunc("/query/median", queryMedian)
+	http.HandleFunc("/query/z", queryZ)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
@@ -58,4 +65,73 @@ func queryCondition(w http.ResponseWriter, r *http.Request) {
 }
 
 
+// studentReq 单个分数上传
+type studentReq struct {
+	Name    string `json:"name"`
+	Class   string `json:"class"`
+	Subject string `json:"subject"`
+	Score   int    `json:"score"`
+}
 
+// addScore 录入单个学生成绩
+func addScore(w http.ResponseWriter, r *http.Request) {
+	l := r.ContentLength
+	body := make([]byte, l)
+	_, err := r.Body.Read(body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	stu := new(studentReq)
+	err = json.Unmarshal(body, stu)
+	if err != nil {
+		log.Fatal(err)
+	}
+	g := new(memory.Grade)
+	g.Name = stu.Name
+	g.Class = stu.Class
+	g.Subject = stu.Subject
+	g.Score = stu.Score
+	table.AddOne(g)
+	fmt.Fprintf(w, "add success")
+}
+
+
+// queryAvg 查询平均分班级 科目
+func queryAvg(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	class := query.Get("class")
+	sub := query.Get("subject")
+
+	average, _ := table.QueryAverage(class, sub)
+	fmt.Fprintf(w, "%s %s 的平均分数为%f", class, sub, average)
+}
+
+// queryMaxOrMin 查询最高分数和最低分数
+func queryMaxOrMin(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	class := query.Get("class")
+	sub := query.Get("subject")
+
+	max, min := table.QueryMaxOrMin(class, sub)
+	fmt.Fprintf(w, "%s %s 的最高分数分数为%f，最低分数为%f", class, sub, max, min)
+}
+
+// queryMedian 查询中位数班级 科目
+func queryMedian(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	class := query.Get("class")
+	sub := query.Get("subject")
+
+	m := table.QueryMedian(class, sub)
+	fmt.Fprintf(w, "%s %s 的中位数分数为%f", class, sub, m)
+}
+
+// queryZ 查询标准差 班级 科目
+func queryZ(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	class := query.Get("class")
+	sub := query.Get("subject")
+
+	m := table.QueryZ(class, sub)
+	fmt.Fprintf(w, "%s %s 的标准差分数为%f", class, sub, m)
+}
--- memory/grade.go
+++ memory/grade.go
@@ -1,12 +1,15 @@
 package memory
 
 import (
+	"math"
 	"sort"
 	"sync"
 )
 
 // lock 读写锁增加表访问安全性和保证一定的性能
-var lock sync.RWMutex
+var (
+	lock sync.RWMutex
+)
 
 // Grade 单条信息
 type Grade struct {
@@ -52,6 +55,13 @@ func (g *SystemTable) Add(grades []*Grade) {
 	g.Grades = append(g.Grades, grades...)
 }
 
+// AddOne 这里写锁
+func (g *SystemTable) AddOne(grade *Grade) {
+	lock.Lock()
+	defer lock.Lock()
+	g.Grades = append(g.Grades, grade)
+}
+
 /**
 实现内置排序接口
 */
@@ -75,4 +85,83 @@ func (g *SystemTable) Less(i, j int) bool { //排序顺序
 }
 func (g *SystemTable) Swap(i, j int) {
 	g.Grades[i], g.Grades[j] = g.Grades[j], g.Grades[i]
+}
+
+// QueryAverage 查询某班级及科目的平均分数
+func (g *SystemTable) QueryAverage(class, subject string) (float64, int) {
+	lock.RLock()
+	defer lock.RUnlock()
+	// 总分数，及人次
+	sumScore, num := 0, 0
+	for _, g := range g.Grades {
+		if g.Class == class && g.Subject == subject {
+			sumScore += g.Score
+			num++
+		}
+	}
+	return float64(sumScore / num), num
+}
+
+// QueryMaxOrMin 查询某班级及科目的最大或者最小分数
+func (g *SystemTable) QueryMaxOrMin(class, subject string) (int, int) {
+	lock.RLock()
+	defer lock.RUnlock()
+	// 最大分数， 最小分数
+	maxScore, minScore := 0, 0
+	for _, g := range g.Grades {
+		if g.Class == class && g.Subject == subject {
+			if g.Score > maxScore {
+				maxScore = g.Score
+			}
+			if minScore < g.Score {
+				minScore = g.Score
+			}
+
+		}
+	}
+	return maxScore, minScore
+}
+
+// QueryMedian 查询某班级及科目的中位数
+func (g *SystemTable) QueryMedian(class, subject string) float64 {
+	lock.RLock()
+	defer lock.RUnlock()
+	s := make([]int, 0, 10)
+	for _, g := range g.Grades {
+		if g.Class == class && g.Subject == subject {
+			s = append(s, g.Score)
+		}
+	}
+	return getMedian(s)
+}
+
+// getMedian 写个冒泡算中位数
+func getMedian(s []int) float64 {
+	length := len(s)
+	for i := 0; i < length/2+1; i++ {
+		for j := length - 1; j > i; j-- {
+			if s[j+1] < s[j] {
+				s[j], s[i] = s[i], s[j]
+			}
+		}
+	}
+	if length%2 == 1 {
+		return float64((s[length/2] + s[length/2+1]) / 2)
+	} else {
+		return float64(s[length/2+1])
+	}
+}
+
+// QueryZ 查询某班级及科目的标准差 s=sqrt(((x1-x)^2 +(x2-x)^2 +......(xn-x)^2)/（n-1))
+func (g *SystemTable) QueryZ(class, subject string) float64 {
+	lock.RLock()
+	defer lock.RUnlock()
+	average, n := g.QueryAverage(class, subject)
+	var sum float64
+	for _, g := range g.Grades {
+		if g.Class == class && g.Subject == subject {
+			sum += math.Abs((average - float64(g.Score)) * (average - float64(g.Score)))
+		}
+	}
+	return math.Sqrt(sum / float64(n-1))
 }
